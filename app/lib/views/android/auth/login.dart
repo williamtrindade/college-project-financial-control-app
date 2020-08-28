a8cae0b204a334c0bf98ca5533e2968dd79e3373
William Trindade <williamtrindade777@gmail.com>
2020-08-28 20:00:53 -0300

    feat(auth/login): Login done

---
+++ app/lib/views/android/auth/login.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tela de login'),
+      ),
+      body: Container(
+        // color: Colors.red,
+        padding: EdgeInsets.symmetric(vertical: 25.0),
+        width: double.infinity,
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  // ignore: missing_return
+                  builder: (BuildContext context) {
+
+                  },
+                //     builder: (context) => Dashboard()
+                )
+            );
+          },
+          color: Colors.green,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0)
+          ),
+          padding: EdgeInsets.all(20.0),
+          child: Text(
+            'Login',
+            style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
---
+++ app/lib/views/android/main.dart
@@ -0,0 +1,11 @@
+import 'package:app/views/android/auth/login.dart';
+import 'package:flutter/material.dart';
+
+class MainAndroidView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginView(),
+    );
+  }
+}
